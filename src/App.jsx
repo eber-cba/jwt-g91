4c115baf8fc563c9af3b92a146c2e74480affec9
Avier-10 <97894530+Avier-10@users.noreply.github.com>
2025-08-28 04:06:37 -0300

    Barra Navegacion
    Proceso de Barra de navegacion con React

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,23 @@ import LoginPage from "./pages/Login";
 import ProfilePage from "./pages/Profile";
 import RegisterWithCustomHooks from "./pages/RegisterWithCustomHooks";
 
+import NavSup from "./Components/NavSup";
+
 const App = () => {
   let user = true;
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterWithCustomHooks />} />
-      <Route path="/profile" element={user ? <ProfilePage /> : <LoginPage />} />
-    </Routes>
+    <div id="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterWithCustomHooks />} />
+        <Route
+          path="/profile"
+          element={user ? <ProfilePage /> : <LoginPage />}
+        />
+      </Routes>
+      <NavSup />
+    </div>
   );
 };
 export default App;
